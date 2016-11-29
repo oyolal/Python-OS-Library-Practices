bffa33557d7336deb1d7d2bd4558639d2d7cc8f7
Omer Faruk Yolal <omeryol42@gmail.com>
2016-11-28 18:43:00 -0600

    fix

--- Practice_2.py
+++ Practice_2.py
@@ -6,9 +6,14 @@ filenames = os.listdir(path)
 print "The directories are: %s"%os.listdir(os.getcwd())
 # Renaming all files to lower cases
 for i in filenames:
-# Take a look .replace() <-- search it on google see how it works
+# Take a look .rename() .replace() <-- search it on google see how it works
 # Do couple exercise yourself
     os.rename(i,i.replace(" ", "-").lower())
 print "Successfully lowered."
 # listing directories after lowering all files
+print "The directories are: %s" %os.listdir(os.getcwd())
+print "Let's fix it back shall we"
+for i in filenames:
+#obviously .capitalize()
+    os.rename(i,i.replace(" ", "-").capitalize())
 print "The directories are: %s" %os.listdir(os.getcwd())
