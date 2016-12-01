d8437b888c262f349e947dcc5be9469ee30a9b68
Omer Faruk Yolal <omeryol42@gmail.com>
2016-12-01 16:19:15 -0600

    practice3

--- Practice_3.py
+++ Practice_3.py
@@ -7,3 +7,10 @@ with open(file_name, 'a'):
         os.utime(file_name, None)
 print os.listdir(os.getcwd())
 print "See now we have file named %s" %file_name
+#Opening the file
+#If upper code didn't create the file you create it with this one too
+practice_4 = open(file_name, 'w')
+#Writing first line
+practice_4.write("import os")
+#Don't forget to close file after working on it
+practice_4.close()
